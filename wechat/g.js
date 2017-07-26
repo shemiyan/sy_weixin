83169d31144e09af9200f77e5a9103232bfe74d5
18770919803 <18770919803@163.com>
2017-07-26 14:38:02 +0800

    已拿到sigture等参数

--- wechat/g.js
+++ wechat/g.js
@@ -1,1 +1,20 @@
+'use strict'
+var sha1=require('sha1');
+module.exports=function(opts){
+    return function *next(){
+        var token=opts.token;
+        var signature=this.query.signature;
+        var nonce=this.query.nonce;
+        var timestamp=this.query.timestamp;
+        var echostr=this.query.echostr;
+        var str=[token,timestamp,nonce].sort().join('');
+        var sha=sha1(str);
 
+        if(sha===signature){
+            this.body=echostr+'';
+            console.log(this);
+        }else{
+            this.body='wrong'
+        }
+    }
+}
